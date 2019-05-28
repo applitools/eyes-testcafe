39fbd3c927b6c1c936da0b60145bb80a57e5e855
Daniel Schwartz <danielsch24@gmail.com>
2019-05-28 11:07:21 +0300

    added clientFunctionWrraper u test

---
+++ tests/unit/clientFunctionWrapper.unit.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const {describe, it} = require('mocha');
+const {expect} = require('chai');
+const makeClientFunctionWrapper = require('../../src/makeClientFunctionWrapper');
+
+describe('makeClientFunctionWrapper', () => {
+  const clientFunctionWrapper = makeClientFunctionWrapper({
+    clientFunctionExecuter: fn => ({
+      with: () => (...args) => fn(...args),
+    }),
+    maxObjectSize: 3,
+    window: {},
+    logger: {log: () => {}},
+  });
+
+  it('works', async () => {
+    const getResult = await clientFunctionWrapper(async () => ({
+      message: 'hello this is a string of some length',
+    }));
+    expect(await getResult()).to.deep.eql({
+      message: 'hello this is a string of some length',
+    });
+  });
+});
