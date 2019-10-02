0f9007bf815630a3740902d3520cc3797173ff69
Daniel Schwartz <danielsch24@gmail.com>
2019-10-02 16:14:24 +0300

    added accessibility to TestResults

--- dist/processPageAndSerialize.js
+++ dist/processPageAndSerialize.js
@@ -519,14 +519,19 @@ module.exports = () => {
   /* global window */
 
   function fetchUrl(url, fetch = window.fetch) {
-    return fetch(url, {
-      cache: 'force-cache',
-      credentials: 'same-origin'
-    }).then(resp => resp.status === 200 ? resp.arrayBuffer().then(buff => ({
-      url,
-      type: resp.headers.get('Content-Type'),
-      value: buff
-    })) : Promise.reject(`bad status code ${resp.status}`));
+    // Why return a `new Promise` like this? Because people like Atlassian do horrible things.
+    // They monkey patched window.fetch, and made it so it throws a synchronous exception if the route is not well known.
+    // Returning a new Promise guarantees that `fetchUrl` is the async function that it declares to be.
+    return new Promise((resolve, reject) => {
+      return fetch(url, {
+        cache: 'force-cache',
+        credentials: 'same-origin'
+      }).then(resp => resp.status === 200 ? resp.arrayBuffer().then(buff => ({
+        url,
+        type: resp.headers.get('Content-Type'),
+        value: buff
+      })) : Promise.reject(`bad status code ${resp.status}`)).then(resolve).catch(err => reject(err));
+    });
   }
 
   var fetchUrl_1 = fetchUrl;
