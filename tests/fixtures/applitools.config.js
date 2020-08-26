20cb529b3706d155d4e464cb090c31a8ab21fd54
tourdedave <dhaeffer@gmail.com>
2020-08-26 11:52:38 -0400

    set test config back to its original values

--- tests/fixtures/applitools.config.js
+++ tests/fixtures/applitools.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   apiKey: process.env.APPLITOOLS_API_KEY,
   isDisabled: false,
-  disableBrowserFetching: true,
   // accessibilityLevel: 'AAA',
   // notifyOnCompletion: true,
   // proxy: {
@@ -12,5 +11,5 @@ module.exports = {
   // showLogs: true,
   // eyesTimeout: 1234,
   // tapDirPath: './',
-  //showLogs: true,
+  // disableBrowserFetching: true,
 };
