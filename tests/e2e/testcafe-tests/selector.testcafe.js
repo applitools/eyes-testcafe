1dbc2e17e0dd3f0bef9e339535dd9c6739430585
tourdedave <dhaeffer@gmail.com>
2020-10-12 10:25:05 -0400

    drop `.only` filter from e2e test

--- tests/e2e/testcafe-tests/selector.testcafe.js
+++ tests/e2e/testcafe-tests/selector.testcafe.js
@@ -5,7 +5,7 @@ import Eyes from '../../../';
 const configPath = path.resolve(__dirname, '../../fixtures/applitools.config.js');
 const eyes = new Eyes({configPath});
 
-fixture.only`Selector`.page`http://localhost:7272/helloworld.html`
+fixture`Selector`.page`http://localhost:7272/helloworld.html`
   .afterEach(async () => eyes.close())
   .after(async () => await eyes.waitForResults(true));
 
